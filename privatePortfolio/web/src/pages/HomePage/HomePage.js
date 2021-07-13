1e5a2132e1caeab7c68ba594297a545ced1e0156
Sean Yang <eiffel205566@hotmail.com>
2021-07-12 21:52:08 -0400

    complete amazingCarousel and  mock typing effect

--- privatePortfolio/web/src/pages/HomePage/HomePage.js
+++ privatePortfolio/web/src/pages/HomePage/HomePage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import randomnizeLetters from './UtilRandomLetter'
 
 const HomePage = () => {
   const [carouselX, setCarouselX] = useState({
@@ -6,7 +7,9 @@ const HomePage = () => {
     extended: false,
     current: 1,
     currentA: 1,
-    diplayedText: '',
+    displayedText: ['x'],
+    randomnizationCount: 0,
+    timeId: null,
   })
 
   const randomPick = () => {
@@ -34,6 +37,64 @@ const HomePage = () => {
     })
   }, [carouselX.translateX])
 
+  //* Mocking typing effect && constant
+  const letters = 'celwmon'
+  const allLetters = [...letters.split('')]
+  const correctLetters = {
+    0: 'w',
+    1: 'e',
+    2: 'l',
+    3: 'c',
+    4: 'o',
+    5: 'm',
+    6: 'e',
+  }
+
+  /*
+  * Mocking random typing effect
+  * welcome is 7 letters, each time Math.random take a letter from allLetters match against correctLetters at index of carouselX.randomnizationCount
+  * if match, ++carouselX.randomnizationCount, once carouselX.randomnizationCount is greater than 6, clear carouselX.timeId 
+  {0: 'w', 1: 'e', 2: 'l', 3:'c', 4:'o', 5: 'm', 6: 'e'}
+  //! [...arr.slice(0, n),'x' ,...arr.slice(n+1 , arr.length)] where n is index
+  */
+  useEffect(() => {
+    if (carouselX.randomnizationCount > 6) {
+      return () => {
+        clearTimeout(carouselX.timeId)
+      }
+    } else {
+      if (
+        correctLetters[carouselX.randomnizationCount] ===
+        carouselX.displayedText[carouselX.randomnizationCount]
+      ) {
+        setCarouselX((state) => {
+          return {
+            ...state,
+            randomnizationCount: ++state.randomnizationCount,
+          }
+        })
+      }
+    }
+
+    setTimeout(() => {
+      setCarouselX((state) => {
+        const randomPick =
+          allLetters[Math.floor(Math.random() * allLetters.length)]
+        return {
+          ...state,
+          displayedText: [
+            ...state.displayedText.slice(0, state.randomnizationCount),
+            randomPick,
+            ...state.displayedText.slice(
+              state.randomnizationCount + 1,
+              state.displayedText.length
+            ),
+          ],
+        }
+      })
+    }, 100)
+  }, [carouselX.displayedText])
+
   return (
     <div className="landingPageContainer min-h-screen w-screen overflow-x-hidden">
       <div
@@ -49,21 +110,34 @@ const HomePage = () => {
           } h-screen w-screen fixed -z-11`}
         ></div>
       ) : null}
-      <section
-        onClick={() => {
-          setCarouselX((state) => {
-            return {
-              ...state,
-              translateX: ++state.translateX,
-              next: state.next === 3 ? 1 : ++state.next,
-            }
-          })
-        }}
-        className="sectionOneContainer h-screen w-screen -z-10"
-      >
-        <div className="text-center h-full w-full bg-gray-300 bg-opacity-40 absolute top-0 left-0 -z-5">
-          <div className="h-full flex flex-col justify-center">
-            <div className="love text-7xl">{carouselX.diplayedText}</div>
+      <section className="sectionOneContainer h-screen w-screen -z-10 flex flex-col justify-center">
+        <div
+          onClick={() => {
+            setCarouselX((state) => {
+              return {
+                ...state,
+                translateX: ++state.translateX,
+                next: state.next === 3 ? 1 : ++state.next,
+              }
+            })
+          }}
+          className="bgUpdateButton select-none h-10 w-48 border rounded-3xl mx-auto hover:text-green-500 z-10 cursor-pointer transform transition-all duration-500"
+        >
+          <div className="horizontalJustifyCenterWrapper h-full flex flex-col justify-center z-10">
+            <span className="love text-xl z-10 text-center">New Wallpaper</span>
+          </div>
+        </div>
+        <div className="gray300overlay text-center h-full w-full bg-gray-300 bg-opacity-40 absolute top-0 left-0 -z-5">
+          <div className="horizontalJustifyCenterWrapper h-full flex flex-col justify-center">
+            <div className="welcomeMessage love text-7xl flex justify-center">
+              {carouselX.displayedText.map((letter, index) => {
+                if (index > 6) {
+                  return null
+                } else {
+                  return <span key={index}>{letter}</span>
+                }
+              })}
+            </div>
           </div>
         </div>
         {/*
--- privatePortfolio/web/src/pages/HomePage/UtilRandomLetter.js
+++ privatePortfolio/web/src/pages/HomePage/UtilRandomLetter.js
@@ -1,11 +1,35 @@
-export default RandomnizeLetters = (func) => {
+// const randomnizeLetters = (func) => {
+//   let timeId = setTimeout(function t() {
+//     const letters = 'abcdefghijklmnopqrstuvwxyz'
+//     const allLetters = [...letters.split(''), letters.toUpperCase().split('')]
+//     func((state) => {
+//       console.log(allLetters[Math.floor(Math.random()) * allLetters.length])
+//       // return {
+//       //   ...state,
+//       //   displayedText: allLeters[Math.floor(Math.random()) * allLetters.length],
+//       // }
+//     })
+//     timeId = setTimeout(t(allLetters), 500)
+//   }, 500)
+//   setTimeout(() => clearTimeout(timeId), 3000)
+// }
+
+const randomnizeLetters = (func) => {
   const letters = 'abcdefghijklmnopqrstuvwxyz'
   const allLetters = [...letters.split(''), letters.toUpperCase().split('')]
+  let timeId = setTimeout(function t() {
+    let randomLetter = allLetters[Math.floor(Math.random() * allLetters.length)]
+    // console.log(allLetters[Math.floor(Math.random() * allLetters.length)])
+    func((state) => {
+      return {
+        ...state,
+        displayedText: randomLetter,
+      }
+    })
 
-  func((state) => {
-    return {
-      ...state,
-      displayedText: state.displayedText,
-    }
-  })
+    timeId = setTimeout(t, 100)
+  }, 100)
+  setTimeout(() => clearTimeout(timeId), 500)
 }
+
+export default randomnizeLetters
